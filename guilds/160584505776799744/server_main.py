f3565f34b76173711cf9079fbf380d7c87867995
steph <maplestephyy@gmail.com>
2023-06-14 00:32:57 -0400

    Removed yeller module from 1605

--- guilds/160584505776799744/server_main.py
+++ guilds/160584505776799744/server_main.py
@@ -4,22 +4,13 @@ import modules.starboard as starboard
 import modules.yeller as yeller
 
 SERVER_ID = str(160584505776799744)
-CONFIG_FILE = 'guilds/' + SERVER_ID + "/serverconfig.ini"
+CONFIG_FILE = 'guilds/' + SERVER_ID + "/server_config.ini"
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
 
 async def handle(message, message_event, client):
-    if message_event == med.MessageEvent.on_message:
-        await on_message(message, message_event, client)
-    elif message_event == med.MessageEvent.on_raw_reaction_add:
+    if message_event == med.MessageEvent.on_raw_reaction_add:
         await on_raw_reaction_add(message, message_event, client)
-
-async def on_message(message, message_event, client):
-    if message.channel.id == config.get('misc','peopleCID'):
-        return
-    else:
-        y = yeller.Yeller(config)
-        await y.handle(message, message_event, client)
 
 async def on_raw_reaction_add(message, message_event, client):
     sb = starboard.Starboard(config)
